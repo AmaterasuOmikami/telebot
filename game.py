53240b488894d42c3c1dd5739cd2155c29c22446
unf1ndabl3 <ak.sergey.ak@yandex.ru>
2024-08-13 02:29:47 +0300

    Add statistic and about_info

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 import random
+from typing import Tuple
 
 
 MAX_TRIES_COUNT = 7
@@ -19,35 +20,35 @@ class Game:
         self.is_started = True
         return INTRO
 
-    def try_guess(self, guess: str) -> str:
+    def try_guess(self, guess: str) -> Tuple[int, str]:
         if not self.is_started:
-            return 'Игра ещё не началась'
+            return 0, 'Игра ещё не началась'
 
         try:
             guess = int(guess)
         except ValueError:
-            return 'Ошибка. Нужно ввести число'
+            return 0, 'Ошибка. Нужно ввести число'
 
-        self._tries += 1
-        
         if self._tries >= self._max_tries:
             self.is_started = False
-            return 'Вы истратили все попытки, начните игру сначала!'
+            return -1, 'Вы истратили все попытки, начните игру сначала!'
+
+        self._tries += 1
 
         if guess == self._number_to_guess:
             self.is_started = False
-            return f'Поздравляю! Вы угадали число {self._number_to_guess} за {self._tries} попыток.'
+            return 1, f'Поздравляю! Вы угадали число {self._number_to_guess} за {self._tries} попыток.'
 
         if guess == 0:
             clue = self._number_to_guess + random.randint(-10, 10)
-            return f'Загаданное число находится в диапазоне +-10 от {clue}'
+            return 0, f'Загаданное число находится в диапазоне +-10 от {clue}'
         if guess < self._number_to_guess:
-            return 'Слишком мало! Попробуйте снова.'
+            return 0, 'Слишком мало! Попробуйте снова.'
         if guess > self._number_to_guess:
-            return 'Слишком много! Попробуйте снова.'
+            return 0, 'Слишком много! Попробуйте снова.'
         if self._tries == self._max_tries:
             self.is_started = False
-            return 'Вы истратили все попытки, попробуйте снова!'
+            return -1, 'Вы истратили все попытки, попробуйте снова!'
 
 
 
